8b02e755fcceac99dcb39f2a8f48692635b164e6
xiaohuo <xiaohuo@voyage.finance>
2023-01-24 13:56:18 +0800

    feat: l0

---
+++ README.md
@@ -0,0 +1,6 @@
+# Ethernaut-Foundry
+
+Solutions and Walkthrough for Openzeppelin's Ethernaut CTF written in Solidity with the help of Foundry.
+
+More levels will be added soon. 
+- [x] To run level 0: `yarn l0`
--- test/TLevel00.sol
+++ test/TLevel00.sol
@@ -13,7 +13,16 @@ contract Attacker is Test {
         console.log("info: %s", info);
         string memory info1 = level0.info1();
         console.log("info1: %s", info1);
-        level0.password();
+        string memory info2 = level0.info2("hello");
+        console.log("info2: %s", info2);
+        uint8 infoNum = level0.infoNum();
+        console.log("infoNum: %d", infoNum);
+        string memory info42 = level0.info42();
+        console.log("info42: %s", info42);
+        string memory theMethodName = level0.theMethodName();
+        console.log("theMethodName: %s", theMethodName);
+        string memory method7123949 = level0.method7123949();
+        console.log("method7123949: %s", method7123949);
         level0.authenticate(level0.password());
         vm.stopBroadcast();
     }
